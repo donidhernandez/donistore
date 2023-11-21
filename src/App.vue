f0d365716d5ea70cacec92966e133bab1e83a23e
Adonai Dominguez <adonaidominguezes@gmail.com>
2023-11-21 19:13:38 +0100

    props

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<script setup></script>
+<script setup>
+import products from '../data/products.json'
+import ProductCard from './components/ProductCard.vue'
+</script>
 
 <template>
   <h1 class="text-3xl font-bold underline">Doni Store</h1>
+  <section class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full gap-y-5 p-20">
+    <ProductCard :product="product" v-for="product in products" :key="product.id" />
+  </section>
 </template>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+const { product } = defineProps(['product'])
+</script>
+
+<template>
+  <div class="max-w-sm w-[350px] p-8 flex flex-col gap-2 shadow-lg rounden-md justify-around">
+    <div class="flex justify-center">
+      <img class="w-full h-full object-cover" :src="product.image" :alt="product.title" />
+    </div>
+
+    <h2 class="text-xl font-semibold">{{ product.title }}</h2>
+    <p class="font-light line-clamp-4">{{ product.description }}</p>
+    <div class="mt-2">
+      <span class="capitalize bg-slate-900 text-white px-5 py-2">{{ product.category }}</span>
+    </div>
+    <button
+      class="bg-indigo-600 text-white py-3 hover:bg-indigo-800 transition-colors duration-300 ease-in cursor-pointer mt-12"
+    >
+      Ver Producto
+    </button>
+  </div>
+</template>
